43f6926e582b4b96479e998d0eae3075fe8035ae
suxin <xloypaypa@gmail.com>
2019-07-10 00:03:01 +0800

    init commit.

---
+++ main.js
@@ -0,0 +1,10 @@
+var add = function (a, b) {
+    return a.name + " + " + b.name + " = " + (a.value + b.value) + ".";
+}
+
+var name = "one";
+var value = 1;
+var a = {name: name, value: value};
+var b = {name: name, value: value};
+
+console.log(add(a, b));
